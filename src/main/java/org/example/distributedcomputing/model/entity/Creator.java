8d6b1f701b67bc1c3e29666e654b1c30a71a5c89
Maxim Kossatyy <max.kossatyy@gmail.com>
2024-05-17 08:39:10 +0500

    add test container
    Signed-off-by: Maxim Kossatyy <max.kossatyy@gmail.com>

--- src/main/java/org/example/distributedcomputing/model/entity/Creator.java
+++ src/main/java/org/example/distributedcomputing/model/entity/Creator.java
@@ -1,8 +1,14 @@
 package org.example.distributedcomputing.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity
 @Table(name = "tbl_creator")
--- src/main/java/org/example/distributedcomputing/model/entity/Tweet.java
+++ src/main/java/org/example/distributedcomputing/model/entity/Tweet.java
@@ -1,11 +1,17 @@
 package org.example.distributedcomputing.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity
 @Table(name = "tbl_tweet")
